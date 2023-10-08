2cd0fdbe1bd0cf5ae573a715b99521a9672070db
Kalana Dananjaya <kalanadananjaya855@gmail.com>
2023-10-08 18:49:48 +0530

    Updated Spring

--- src/main/java/AppInitializer.java
+++ src/main/java/AppInitializer.java
@@ -2,11 +2,22 @@ import bean.SpringBean;
 import config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.sql.SQLOutput;
+
 public class AppInitializer {
     public static void main(String[] args) {
+
+        //meka run wenne awasanetama. jvm ek shutdown wenna kalin mokk hri process ekk
+        // wennna onanm meka athule tma krnne
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("JVM is about to shutdown");
+            }
+        }));
+
         //Conventions
         //==============
-
         // Create a BeanFactory and application context
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
         ctx.register(AppConfig.class);
