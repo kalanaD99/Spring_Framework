88370ad79fed36f69e215a0fac6cba566e740716
Kalana Dananjaya <kalanadananjaya855@gmail.com>
2023-10-08 12:52:23 +0530

    Updated Spring

--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -3,8 +3,12 @@ package config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 @ComponentScan(basePackages = "bean")
 public class AppConfig {
+    public  AppConfig(){
+        System.out.println("App config object created");
+    }
 
 }
