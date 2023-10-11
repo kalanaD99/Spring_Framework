b8e96c141106a901d483fa04dc47a759290e5161
Kalana Dananjaya <kalanadananjaya855@gmail.com>
2023-10-11 23:43:01 +0530

    Updated Spring

--- _05_Configurations/src/main/java/AppInitializer.java
+++ _05_Configurations/src/main/java/AppInitializer.java
@@ -1,8 +1,12 @@
+import Config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AppInitializer {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.register();
+        context.register(AppConfig.class);
+        context.refresh();
+        context.registerShutdownHook();
+
     }
 }
